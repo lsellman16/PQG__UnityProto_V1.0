eba6a747f22668e7d1fe7a65273f81404bf364da
lsellman16 <99856297+lsellman16@users.noreply.github.com>
2023-01-26 17:38:31 +1100

    Work on movement input
    Testing movement in 3 separate scenes to see which is the right one

---
+++ PQGUnityProto_V1.0/Assets/CameraMovement_Type1/ClickMovement.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClickMovement : MonoBehaviour
+{
+    [SerializeField]
+    private InputAction mouseClickAction;
+}
