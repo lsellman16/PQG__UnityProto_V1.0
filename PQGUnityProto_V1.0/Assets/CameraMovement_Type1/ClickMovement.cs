480f1fdb00fac39ac0d2a63fd9c928b283eadb21
lsellman16 <99856297+lsellman16@users.noreply.github.com>
2023-01-29 12:10:05 +1100

    Work on movement
    NOT DONE DO NOT ADVANCED UNTIL FIXED

--- PQGUnityProto_V1.0/Assets/CameraMovement_Type1/ClickMovement.cs
+++ PQGUnityProto_V1.0/Assets/CameraMovement_Type1/ClickMovement.cs
@@ -5,6 +5,29 @@ using UnityEngine.InputSystem;
 
 public class ClickMovement : MonoBehaviour
 {
-    [SerializeField]
-    private InputAction mouseClickAction;
+    [SerializeField] private InputAction mouseClickAction;
+    
+    private void Awake() 
+    {
+        mainCamera = Camera.Main;
+    }
+
+    private void OnEnable() 
+    {
+        mouseClickAction.Enable();
+        mouseClickAction.performed += Move;    
+    }
+
+    private void OnDisable() {
+        mouseClickAction.performed -= Move;
+        mouseClickAction.Disable();
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+       Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+       if (Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider) {
+
+       }
+    }
 }
